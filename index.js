80a967a5f5cbb9f6ff137b66b5fc894badba33ed
slawciu <slawomir.rudawski@gmail.com>
2020-03-11 15:44:22 +0100

    Logging branch name

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  console.log(`The branch: ${payload.ref}`)
 } catch (error) {
   core.setFailed(error.message);
 }
