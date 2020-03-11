09c6c8c25ab3144095a42d54712820f92a05428c
slawciu <slawomir.rudawski@gmail.com>
2020-03-11 16:21:09 +0100

    Set label to issue

--- dist/index.js
+++ dist/index.js
@@ -24420,14 +24420,28 @@ function checkMode (stat, options) {
 
 const core = __webpack_require__(83);
 const github = __webpack_require__(492);
+const fetch = __webpack_require__(619);
 
 try {
-  const branchName = core.getInput('branch-name');
-  console.log(`Runs on ${branchName}!`);
+  const gitlabUrl = core.getInput('gitlab-url');
+  const label = core.getInput('label');
+  const projectId = core.getInput('project-id');
+  const accessToken = core.getInput('access-token');
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  console.log(`The branch: ${github.context.payload.ref}`);
+  const ref = github.context.payload.ref;
+  const issueId = ref.substring(ref.indexOf(`heads/`)+`heads/`.length, ref.indexOf('-'));
+  
+  console.log(`The branch: ${ref}`);
+
+  fetch(`https://${gitlabUrl}/${projectId}/issues/${issueId}?labels=${label}`, {
+    method: 'PUT',
+    headers: {
+      "Private-Token": accessToken
+    },
+  });
+
 } catch (error) {
   core.setFailed(error.message);
 }
--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fetch = require('node-fetch');
 
 try {
-  const branchName = core.getInput('branch-name');
-  console.log(`Runs on ${branchName}!`);
+  const gitlabUrl = core.getInput('gitlab-url');
+  const label = core.getInput('label');
+  const projectId = core.getInput('project-id');
+  const accessToken = core.getInput('access-token');
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
-  console.log(`The branch: ${github.context.payload.ref}`);
+  const ref = github.context.payload.ref;
+  const issueId = ref.substring(ref.indexOf(`heads/`)+`heads/`.length, ref.indexOf('-'));
+  
+  console.log(`The branch: ${ref}`);
+
+  fetch(`https://${gitlabUrl}/${projectId}/issues/${issueId}?labels=${label}`, {
+    method: 'PUT',
+    headers: {
+      "Private-Token": accessToken
+    },
+  });
+
 } catch (error) {
   core.setFailed(error.message);
 }
