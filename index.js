874c1564d7a41a35adacc1ffa60112d070aeeab4
slawciu <slawomir.rudawski@gmail.com>
2020-03-11 16:47:43 +0100

    Removed access token and console logs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fetch = require('node-fetch');
+
 const main = async () => {
   try {
     const gitlabUrl = core.getInput('gitlab-url');
@@ -8,13 +9,9 @@ const main = async () => {
     const projectId = core.getInput('project-id');
     const accessToken = core.getInput('access-token');
     
-    console.log(gitlabUrl, label, projectId, accessToken);
-    
-    // Get the JSON webhook payload for the event that triggered the workflow
     const ref = github.context.payload.ref;
     const issueId = ref.substring(ref.indexOf(`heads/`)+`heads/`.length, ref.indexOf('-'));
     
-    console.log(`https://${gitlabUrl}/api/v4/projects/${projectId}/issues/${issueId}?labels=${encodeURIComponent(label)}`)
     await fetch(`https://${gitlabUrl}/api/v4/projects/${projectId}/issues/${issueId}?labels=${encodeURIComponent(label)}`, {
       method: 'PUT',
       headers: {
