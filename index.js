564cb1aea144a7ff0e94e7edc41bd6d2bec62b7e
slawciu <slawomir.rudawski@gmail.com>
2020-03-11 16:43:30 +0100

    run async function

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fetch = require('node-fetch');
+const main = async () => {
+  try {
+    const gitlabUrl = core.getInput('gitlab-url');
+    const label = core.getInput('label-to-apply');
+    const projectId = core.getInput('project-id');
+    const accessToken = core.getInput('access-token');
+    
+    console.log(gitlabUrl, label, projectId, accessToken);
+    
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const ref = github.context.payload.ref;
+    const issueId = ref.substring(ref.indexOf(`heads/`)+`heads/`.length, ref.indexOf('-'));
+    
+    console.log(`The branch: ${ref}`);
 
-try {
-  const gitlabUrl = core.getInput('gitlab-url');
-  const label = core.getInput('label-to-apply');
-  const projectId = core.getInput('project-id');
-  const accessToken = core.getInput('access-token');
-  
-  console.log(gitlabUrl, label, projectId, accessToken);
-  
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const ref = github.context.payload.ref;
-  const issueId = ref.substring(ref.indexOf(`heads/`)+`heads/`.length, ref.indexOf('-'));
-  
-  console.log(`The branch: ${ref}`);
+    await fetch(`https://${gitlabUrl}/${projectId}/issues/${issueId}?labels=${encodeURIComponent(label)}`, {
+      method: 'PUT',
+      headers: {
+        "Private-Token": accessToken
+      },
+    });
 
-  await fetch(`https://${gitlabUrl}/${projectId}/issues/${issueId}?labels=${encodeURIComponent(label)}`, {
-    method: 'PUT',
-    headers: {
-      "Private-Token": accessToken
-    },
-  });
-
-} catch (error) {
-  core.setFailed(error.message);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+main();
